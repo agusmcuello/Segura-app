bb8972e6e2fc62589e98b398da6fea72ce677303
Agustin Martinez <agustinmcuello@gmail.com>
2022-12-12 11:58:59 -0300

    final commit

--- src/components/Data/Data.jsx
+++ src/components/Data/Data.jsx
@@ -6,7 +6,7 @@ const Data = () => {
         <div className='data'>
             <div className="container">
                 <div className="content">
-                    <h2><span>Data</span> Recovery</h2>
+                    <h2><span>Recuperación</span> de Datos</h2>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi officiis unde perferendis voluptas laborum aliquam culpa officia a maiores porro. Consequuntur officia corporis aliquam numquam, fuga quisquam illo. Obcaecati, voluptatum.</p>
                     <div>
                         <button>Sign Up</button>
